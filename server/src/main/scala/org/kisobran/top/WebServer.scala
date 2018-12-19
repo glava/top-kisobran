0cbbfa5dabebefb588ca83942c7a2fb74ec62287
Andreas Drobisch <github@drobisch.com>
2018-12-19 23:33:41 +0100

    remove center columns, use mx-auto to center, add videos to h2 entries

--- server/src/main/scala/org/kisobran/top/WebServer.scala
+++ server/src/main/scala/org/kisobran/top/WebServer.scala
@@ -28,6 +28,12 @@ object WebServer extends LoggingSupport {
     implicit val ex = ExecutionContext.global
     // todo: move to liqubase
     if (dbProfile == H2Profile) {
+      val videos = Seq(
+        "https://www.youtube.com/embed/Yr-cnSQDhVQ", // goran series
+        "https://www.youtube.com/embed/SlbVgjFvE3I", // Peggy Gou
+        "https://www.youtube.com/embed/vIh7IsgauRQ" // worst darts
+      )
+
       statsRepository.ensureTablesPresent(true)
       topListRepository.ensureTablesPresent(true).map { _ =>
         (1 to 20).map { t =>
@@ -35,7 +41,8 @@ object WebServer extends LoggingSupport {
           (1 to 10).map { i => Entry(s"artist${i}", s"song${i}", i, i) },
           s"${t}-best-list",
             true,
-            2018
+            2018,
+            Some(videos(t % videos.size) + s"?$t")
           )
         }
       }
