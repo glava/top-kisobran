2205be85a3a7a7d4403215d93eb7dcac52adb8f4
Goran Ojkić <gojkic@gmail.com>
2020-01-10 20:16:25 +0100

    Quick spammer fix

--- server/src/main/scala/org/kisobran/top/routes/VoteRoutes.scala
+++ server/src/main/scala/org/kisobran/top/routes/VoteRoutes.scala
@@ -11,37 +11,51 @@ import org.kisobran.top.util.LoggingSupport
 import org.kisobran.top.twirl.Implicits._
 import play.twirl.api.HtmlFormat
 
+import scala.collection.immutable
 import scala.concurrent.{ExecutionContext, Future}
 
 class VoteRoutes(topListRepository: TopListRepository,
                  statsRepository: StatsRepository)(implicit executionContext: ExecutionContext) extends Directives with LoggingSupport {
 
-  val route: Route = pathPrefix("vote") {
-    post(formFieldMap(formContent => complete(votePage(formContent))))
-  }
+  case class VoteForm(title: String, entries: Seq[Entry], email: Option[String])
 
-  private def votePage(formContent: Map[String, String]): Future[Future[HtmlFormat.Appendable]] = {
-    val title = formContent.getOrElse("listName", s"untilted-${UUID.randomUUID()}")
-    val entries = (1 to 10).map {
+  def parse(formContent: Map[String, String]): Option[VoteForm] = {
+    val title: String = formContent.getOrElse("listName", s"untilted-${UUID.randomUUID()}")
+    val entries: immutable.IndexedSeq[Entry] = (1 to 10).map {
       case 1 => Entry(formContent(s"inputArtist1"), formContent(s"inputSong1"), position = 1, points = 10)
       case 2 => Entry(formContent(s"inputArtist2"), formContent(s"inputSong2"), position = 2, points = 9)
       case index@_ => Entry(formContent(s"inputArtist$index"), formContent(s"inputSong$index"), position = index, points = 8)
     }
+    if (entries.exists(e => e.artist.contains("""http://""") || e.song.contains("http://") || e.song.contains("https://") || e.artist.contains("https://"))) {
+      None
+    } else Some(VoteForm(title, entries, formContent.get("userEmail")))
+  }
 
-    log.info(title)
-    log.info(entries.toString)
+  val route: Route = pathPrefix("vote") {
+    post(formFieldMap(
+      formContent => {
+        val page: Future[Future[HtmlFormat.Appendable]] = parse(formContent).map(votePage).getOrElse(
+          Future.successful(Future.successful(org.kisobran.top.html.voted.render()))
+        )
+        complete(page)
+      }
+    ))
+  }
+
+  private def votePage(voteForm: VoteForm): Future[Future[HtmlFormat.Appendable]] = {
+    log.info(voteForm.title)
+    log.info(voteForm.entries.toString)
 
     topListRepository.createTopList(
-      formContent.get("userEmail"),
-      entries,
-      title,
+      voteForm.email,
+      voteForm.entries,
+      voteForm.title,
       enabled = false,
       Configuration.currentYear
     ).map { topListEntry =>
       val insertOperation: Future[Seq[Stats]] = topListEntry.map { topList =>
-        statsRepository.createStats(topList.id, entries)
+        statsRepository.createStats(topList.id, voteForm.entries)
       }.getOrElse(Future.successful(Seq.empty))
-
       insertOperation.map { _ =>
         org.kisobran.top.html.lista.render(topListEntry, message = true, admin = false, Seq.empty, Seq.empty)
       }
