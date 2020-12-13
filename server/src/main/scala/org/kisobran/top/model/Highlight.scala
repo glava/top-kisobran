4531cc191efab6353f5feb4a493394c43b2a173e
Goran Ojkić <gojkic@gmail.com>
2020-12-13 09:54:54 +0100

    Update highlight

--- server/src/main/scala/org/kisobran/top/model/Highlight.scala
+++ server/src/main/scala/org/kisobran/top/model/Highlight.scala
@@ -17,8 +17,8 @@ object Highlight {
 
   private val goran = Highlight(
     "Vincent Van Gogo",
-    yt = EmbeddedUtil.toEmbedded("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/616622964"),
-    "SAULT jer se i dalje ložim ko tinejdžer na mistične i drske bendove"
+    yt = EmbeddedUtil.toEmbedded("https://www.youtube.com/embed/hp0KiJs0S5g"),
+    "Cleo Sol je sve što mi je trebalo ove godine"
   )
 
 //  private val vidak = Highlight(
@@ -27,11 +27,11 @@ object Highlight {
 //    "Ja sam Vidak ide mi se u džihad."
 //  )
 
-//  private val milan = Highlight(
-//    "Midnight Citizen",
-//    yt = "https://www.youtube.com/embed/4sk0uDbM5lc",
-//    "Ne postoji bolja pesma za vetar, sunce, oluju, sneg, noć, trčanje, tunel, praznu ulicu, samoću, hangar, bogove. Jon je maestro koji se uvlači pod kožu i izaziva jezu kroz svaku poru."
-//  )
+  private val milan = Highlight(
+    "Midnight Citizen",
+    yt = "https://www.youtube.com/embed/qS_o7XpnEqA",
+    "2020. godina nam je po mnogo čemu ostala dužna, ali kada se sve vrati u neku vrstu normale, prva pesma za plesni podijum će nesumnjivo biti pobednička himna Jessie Ware."
+  )
 
   private val maki = Highlight(
     "Benjd",
@@ -39,11 +39,11 @@ object Highlight {
     "Ovo je dobra stvar koju možete poslušati ako rešite da odradite kvalitetnu solo šetnjicu sa sluškama u ušima!"
   )
 
-//  private val kimi = Highlight(
-//    "Kimi, idejni tvorac top deset akcije",
-//    yt = EmbeddedUtil.toEmbedded("https://youtu.be/mMDeJfJttUU"),
-//    "Samo elektronika i sajentologija"
-//  )
+  private val kimi = Highlight(
+    "Kimi, idejni tvorac top deset akcije",
+    yt = EmbeddedUtil.toEmbedded("https://www.youtube.com/embed/lo4KMGiy--Y"),
+    "Bar je za muziku bila dobra godina"
+  )
   
   private val mimi = Highlight(
     "Mimi, padavičar iz beograda",
@@ -69,7 +69,7 @@ object Highlight {
     "Malo ko je u stanju da peva o ovim stvarima u 2020 a da to ne zvuči otrcano.\nThe Magnetic Fields jesu. "
   )
 
-  val items = Seq(mimi, whisper)
+  val items = Seq(mimi, whisper, milan, goran, kimi)
 
   def element() = {
     val random = new Random
